467ffbf45a18b18dca1e8163af09ebdd0f284d41
resistantJs <49294235+resistantJs@users.noreply.github.com>
2023-10-20 11:04:04 -0700

    Update main.py

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -17,6 +17,20 @@ def draw():
   p5.noStroke()
   p5.fill(150)
   p5.ellipse(150, 150, circle_size, circle_size)
+  p5.push()
+  
+  #set angle var to int of data
+  angle = int(data)
+  #move to middle of canvas
+  p5.translate(pt.width/2, pt.height/2)
+  #rotate canvas with angle converted from degrees to radians:
+  p5.rotate(p5.radians(angle))
+  # change mode to drawr rectangels from center:
+  p5.rectMode(p5.Center)
+  #draw rect at coordinate 0, 0 and 100 width and height
+  p5.rect(0,0,100,100)
+  #rectore graphical transformation
+  p5.pop()
 
 
   
