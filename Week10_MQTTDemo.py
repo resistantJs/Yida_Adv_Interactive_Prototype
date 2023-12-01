262a5785f394124c7cabd7081ee5606388195af1
resistantJs <49294235+resistantJs@users.noreply.github.com>
2023-12-01 09:23:34 -0800

    Test Wait

--- Week10_MQTTDemo.py
+++ Week10_MQTTDemo.py
@@ -3,14 +3,19 @@ import M5
 from M5 import *
 from umqtt import *
 from hardware import *
+import time
 
 
 mqtt_client = None
+adc = None
+adc_val = None
 username = 'ResistantJs'
-password = 'Password Here'
+password = ''
+mqtt_timer = 0
+
 
 def setup():
-  global mqtt_client, username, password
+  global mqtt_client, username, password, adc, adc_val
 
   M5.begin()
   mqtt_client = MQTTClient('testjason',
@@ -18,18 +23,28 @@ def setup():
                            port=1883,
                            user= username,
                            password= password,
-                           keepalive=0)
+                           keepalive=3000)
   
   mqtt_client.connect(clean_session=True)
+  #config adc
+  adc = ADC(Pin(8), atten = ADC.ATTN_11DB)
 
 
 def loop():
-  global mqtt_client
+  global mqtt_client, mqtt_timer, adc, adc_val
   M5.update()
   if BtnA.wasPressed():
-    print("Button Press")
-    mqtt_client.publish('ResistantJs/feeds/button-feed', 'Button Press', qos=0)
-
+    print("ON")
+    mqtt_client.publish('ResistantJs/feeds/button-feed', 'ON', qos=0)
+  
+  if BtnA.wasReleased():
+    print("OFF")
+    mqtt_client.publish('ResistantJs/feeds/button-feed', 'OFF', qos=0)
+  if(time.ticks_ms() > mqtt_timer + 2500):
+    mqtt_client.publish('ResistantJs/feeds/button-feed', 'waiting', qos=0)
+    print('waiting for data')
+    #update timer
+    mqtt_timer = time.ticks_ms()
 
 if __name__ == '__main__':
   try:
